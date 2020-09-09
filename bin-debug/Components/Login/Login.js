57378497fe506259c3e727d7a8a93e9ad7ef994f
Soe Htun <shtun106@gmail.com>
2020-09-09 14:36:05 +0630

    change button color

--- bin-debug/Components/Login/Login.js
+++ bin-debug/Components/Login/Login.js
@@ -1,17 +1,14 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var LoginUI = /** @class */ (function (_super) {
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var __extends = this && this.__extends || function __extends(t, e) { 
+ function r() { 
+ this.constructor = t;
+}
+for (var i in e) e.hasOwnProperty(i) && (t[i] = e[i]);
+r.prototype = e.prototype, t.prototype = new r();
+};
+var LoginUI = (function (_super) {
     __extends(LoginUI, _super);
     function LoginUI() {
         var _this = _super.call(this) || this;
@@ -41,4 +38,4 @@ var LoginUI = /** @class */ (function (_super) {
     };
     return LoginUI;
 }(eui.Component));
-//# sourceMappingURL=Login.js.map
+__reflect(LoginUI.prototype, "LoginUI");
